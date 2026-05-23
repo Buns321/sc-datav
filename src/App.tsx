da87595d88904ebc5bbacfba2b0ec889a53e21fd
Buns321 <buns_music@outlook.com>
2026-05-24 06:52:38 +0800

    feat: 添加DataCenter路由

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Demo0 from "./pages/Demo0";
 import Demo1 from "./pages/Demo1";
 import Demo2 from "./pages/Demo2";
 import Demo3 from "./pages/Demo3";
+import DataCenter from "./pages/DataCenter";
 
 const Index = lazy(() => import("./pages/Index/index"));
 
@@ -36,6 +37,7 @@ function App() {
         <Route path="/demo1" element={<Demo1 />} />
         <Route path="/demo2" element={<Demo2 />} />
         <Route path="/demo3" element={<Demo3 />} />
+        <Route path="/datacenter" element={<DataCenter />} />
       </Routes>
     </div>
   );
